12293300a9c4715e0cd6b21f00ac5d0433ed4bb2
Bohdan Anderson <117282536+Bohdan-Anderson-Sanofi@users.noreply.github.com>
2023-01-09 20:54:07 -0500

    refreshed pre commit

--- library/core-components/components/button/__tests__/button.test.tsx
+++ library/core-components/components/button/__tests__/button.test.tsx
@@ -4,40 +4,24 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
-import { RadiusButton, RadiusButtonProps } from '..';
+import { RadiusButton } from '..';
 
 expect.extend(toHaveNoViolations);
 
 describe('<RadiusButton />', () => {
-  const props: RadiusButtonProps<'button'> = {
-    children: 'Button',
-    size: 'medium',
-    variant: 'secondary',
-    disabled: false,
-    onClick: jest.fn(),
-  };
-
-  const propsWithHref: RadiusButtonProps<'a'> = {
-    href: 'https://www.google.com',
-    as: 'a',
-    children: 'Button',
-    size: 'medium',
-    variant: 'primary',
-    onClick: jest.fn(),
-  };
   test('should render', () => {
-    const { getByRole } = render(<RadiusButton {...props} />);
+    const { getByRole } = render(<RadiusButton />);
     expect(getByRole('button')).toBeInTheDocument();
   });
 
   test('should render an anchor tag when as is set to "a"', () => {
-    const { getByRole } = render(<RadiusButton {...propsWithHref} />);
-    expect(getByRole('link')).toHaveAttribute('href', propsWithHref.href);
+    const { getByRole } = render(<RadiusButton as={'a'} href="#helloworld" />);
+    expect(getByRole('link')).toHaveAttribute('href', '#helloworld');
   });
 
   test('should have no accessibility violations', async () => {
     const { getByRole } = render(
-      <RadiusButton {...props} aria-label="Favourite button" />
+      <RadiusButton aria-label="Favourite button" />
     );
     expect(await axe(getByRole('button'))).toHaveNoViolations();
   });
