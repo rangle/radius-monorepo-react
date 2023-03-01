aa2269451ed8bd39d3299ae5f40b2415c604f1cd
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-03-01 16:39:43 -0500

    remove unused line in test

--- library/core-components/components/typography/__tests__/typography.test.tsx
+++ library/core-components/components/typography/__tests__/typography.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { toHaveNoViolations, axe } from 'jest-axe';
 
