6e910e084520310d013748f0163289bb1be72e69
Stephanie Zeng <stephanie.zeng@rangle.io>
2023-03-07 11:40:31 -0500

    Fix hero component button

--- apps/demo/app/routes/index.tsx
+++ apps/demo/app/routes/index.tsx
@@ -4,7 +4,7 @@ import { RadiusHero } from '@rangle/radius-react-core-components';
 export default function Index() {
   return (
     <>
-      <RadiusHero title="Design systems, Accelerated" eyebrow="For business" buttonLabel="Get info" imageSrc="images/header-development.png"/>
+      <RadiusHero title="Design systems, Accelerated" eyebrow="For business" buttonLabel="Get info" imageSrc="images/header-development.png" ctaUrl="/posts"/>
     </>
   );
 }
