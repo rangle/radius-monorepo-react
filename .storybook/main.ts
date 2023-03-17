3124425cb2510b0cc91b08169636cd94c817188b
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-03-17 13:18:24 -0400

    migrate to storybook 7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,39 +1,22 @@
 import { Config } from '@storybook/addons';
-
 const versions = process.env.VERSION?.split('.') || ['0', '0', '0'];
-
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
-
   // the location of where storybook should look for it's stories
-  stories: [
-    '../library/**/*.stories.mdx',
-    '../library/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-
+  stories: ['../library/**/*.mdx', '../library/**/*.stories.@(js|jsx|ts|tsx)'],
   // Add assets, these files will be added to the root of the build (/fonts.css)
-  staticDirs: [
-    '../shared/fonts',
-    '../shared/assets',
-    '../library/foundations/generated',
-  ],
-
-  addons: [
-    'storybook-version',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-pseudo-states',
-    '@geometricpanda/storybook-addon-badges',
-  ],
-  framework: '@storybook/react',
-
+  staticDirs: ['../shared/fonts', '../shared/assets', '../library/foundations/generated'],
+  addons: ['storybook-version', '@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-pseudo-states', '@geometricpanda/storybook-addon-badges', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
   // We add versions globally from the build env
   // Access the variable with process.env.COMPONENT_VERSION
   env: (config: Config) => ({
     ...config,
-    COMPONENT_VERSION: versions,
+    COMPONENT_VERSION: versions
   }),
+  docs: {
+    autodocs: true
+  }
 };
