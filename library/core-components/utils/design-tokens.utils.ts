4f55c787a759956e8f7130979a34792f49c6c37f
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-04-18 12:35:03 -0400

    add multiple token capability to renderCSSProp

--- library/core-components/utils/design-tokens.utils.ts
+++ library/core-components/utils/design-tokens.utils.ts
@@ -1,12 +1,26 @@
-import type {
-  CSSExpression,
-  RadiusTokens,
-} from '@rangle/radius-foundations/generated/design-tokens.types';
+import type { CSSExpression } from '@rangle/radius-foundations/generated/design-tokens.types';
 
 // TODO: move this function to `library/foundations/src/utils/design-tokens.utils.ts` - this is temporary while we figure out how to fix the build failure caused by importing it (see related issue: https://rangle.atlassian.net/browse/R20-231)
 export const renderCSSProp = (
-  prop: RadiusTokens | { css: CSSExpression } | undefined
+  prop: string | { css: CSSExpression } | undefined
 ) => {
   if (!prop) return undefined;
-  return typeof prop === 'string' ? `var(${prop})` : prop.css;
+  if (typeof prop !== 'string') return prop.css;
+  return PrefixWithVar(prop);
+};
+
+/**
+ * Takes a string representing one or more tokens and adds the `var()` prefix to each.
+ * This allows for multiple tokens to be used in a single CSS property.
+ * @example
+ * PrefixWithVar('--a') // returns 'var(--a)'
+ * PrefixWithVar('--a --b') // returns 'var(--a) var(--b)'
+ */
+const PrefixWithVar = (prop: string) => {
+  // split the string into an array of strings, separated by spaces
+  const splitProp = prop.split(' ');
+  // add the `var(` prefix to each item in the array
+  const prefixedProp = splitProp.map((item) => `var(${item})`);
+  // join the array back into a string, separated by spaces, and return it
+  return prefixedProp.join(' ');
 };
