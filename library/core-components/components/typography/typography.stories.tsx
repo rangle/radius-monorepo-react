6036494f364ff4f9856dc423f3f24f7fbc31c1ab
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-04-24 09:41:46 -0400

    make storybook controls dynamic

--- library/core-components/components/typography/typography.stories.tsx
+++ library/core-components/components/typography/typography.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import { Meta, StoryObj, ArgTypes, Args } from '@storybook/react';
+import { radiusTokens } from '@rangle/radius-foundations/generated/design-tokens.constants';
 // import { BADGE } from '@geometricpanda/storybook-addon-badges';
 import { Typography } from './typography';
+import { flattenObject } from '../../utils';
 
 const meta: Meta<typeof Typography> = {
   component: Typography,
@@ -24,95 +25,50 @@ const meta: Meta<typeof Typography> = {
     align: {
       defaultValue: 'left',
     },
-    color: {
-      defaultValue: '--color-text-on-base-primary',
-      options: [
-        '--color-text-on-base-primary',
-        '--color-text-on-base-secondary',
-        '--color-text-on-base-accent',
-        '--color-text-on-base-disabled',
-        '--color-text-inverse-primary',
-        '--color-text-inverse-secondary',
-        '--color-text-inverse-accent',
-        '--color-text-on-subtle-primary',
-        '--color-text-on-subtle-secondary',
-        '--color-text-on-subtle-accent',
-        '--color-text-on-subtle-disabled',
-        '--color-text-on-accent-primary',
-        '--color-text-on-accent-secondary',
-        '--color-text-on-accent-disabled',
-        '--color-text-primary-action-default',
-        '--color-text-primary-action-hover',
-        '--color-text-primary-action-active',
-        '--color-text-primary-action-focus',
-        '--color-text-primary-action-disabled',
-        '--color-text-secondary-action-default',
-        '--color-text-secondary-action-hover',
-        '--color-text-secondary-action-active',
-        '--color-text-secondary-action-focus',
-        '--color-text-secondary-action-disabled',
-        '--color-text-tertiary-action-default',
-        '--color-text-tertiary-action-hover',
-        '--color-text-tertiary-action-active',
-        '--color-text-tertiary-action-focus',
-        '--color-text-tertiary-action-disabled',
-      ],
-      control: 'select',
+    fill: {
+      options: flattenObject(radiusTokens.component.color),
     },
     font: {
-      defaultValue: '--typography-body-md',
-      options: [
-        '--typography-heading-xxl',
-        '--typography-heading-xl',
-        '--typography-heading-lg',
-        '--typography-heading-md',
-        '--typography-heading-sm',
-        '--typography-heading-xs',
-        '--typography-body-xl',
-        '--typography-body-md',
-        '--typography-body-sm',
-        '--typography-link-sm',
-        '--typography-btn-lg',
-        '--typography-btn-md',
-        '--typography-btn-sm',
-      ],
-      control: 'select',
+      options: flattenObject({
+        core: radiusTokens.core.typography,
+        semantic: radiusTokens.semantic.typography,
+        component: radiusTokens.component.typography,
+      }),
     },
   } as ArgTypes, // TODO: Fix argTypes inference (broken due to polymorphic implementation?). This assertion is a workaround.
   args: {
     children: 'Hello World!',
     as: 'p',
     align: 'left',
-    color: '--color-text-on-base-primary',
-    font: '--typography-body-md',
+    font: radiusTokens.core.typography.desktop.body.md,
   } as Args, // TODO: Fix args inference (broken due to polymorphic implementation?). This assertion is a workaround.
 };
 
 export default meta;
 type Story = StoryObj<typeof Typography>;
 
-export const Basic: Story = {};
+export const Default: Story = {};
 
-export const Example: Story = {
-  render: () => (
-    <div>
-      <Typography font="--typography-heading-xl">Title</Typography>
-      <Typography color="--color-text-on-base-accent">
-        Some body text. Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Fuga, blanditiis.
-      </Typography>
-      <Typography
-        font="--typography-body-sm"
-        color="--color-text-on-base-secondary"
-        align="right"
-      >
-        *Footnote
-      </Typography>
-    </div>
-  ),
-  parameters: {
-    controls: {
-      disable: true,
-    },
-  },
-};
+// export const Example: Story = {
+//   render: () => (
+//     <div>
+//       <Typography font={radiusTokens.semantic.typography.desktop.}>Title</Typography>
+//       <Typography fill="--color-text-on-base-accent">
+//         Some body text. Lorem ipsum dolor sit, amet consectetur adipisicing
+//         elit. Fuga, blanditiis.
+//       </Typography>
+//       <Typography
+//         font="--typography-body-sm"
+//         fill="--color-text-on-base-secondary"
+//         align="right"
+//       >
+//         *Footnote
+//       </Typography>
+//     </div>
+//   ),
+//   parameters: {
+//     controls: {
+//       disable: true,
+//     },
+//   },
+// };
