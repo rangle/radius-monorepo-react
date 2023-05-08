cc243521dd087de60c88b8d63bcdf7c3272428ca
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-05-08 16:08:09 -0400

    update tests

--- library/core-components/components/link-icon/__tests__/link-icon.test.tsx
+++ library/core-components/components/link-icon/__tests__/link-icon.test.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { axe, toHaveNoViolations } from 'jest-axe';
+import { AccountCircle } from '@rangle/radius-foundations/generated/icons';
 
 import { RadiusLinkIcon } from '..';
+import { radiusTokens } from '@rangle/radius-foundations/generated/design-tokens.constants';
 
 expect.extend(toHaveNoViolations);
 
 describe('<RadiusLinkIcon />', () => {
+  test('should render', () => {
+    const { getByRole } = render(
+      <RadiusLinkIcon
+        href="#"
+        icon={AccountCircle}
+        size={radiusTokens.component.sizing.linkIcon.small}
+      />
+    );
+    expect(getByRole('link')).toBeInTheDocument();
+  });
+
   test('should have no accessibility violations', async () => {
-    const { container } = render(<RadiusLinkIcon />);
+    const { container } = render(
+      <RadiusLinkIcon
+        href="#"
+        icon={AccountCircle}
+        size={radiusTokens.component.sizing.linkIcon.small}
+      />
+    );
     expect(await axe(container)).toHaveNoViolations();
   });
 });
