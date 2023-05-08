309a5c2266ab0ade440e6b88045cefd407ae688b
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-05-08 16:05:05 -0400

    add variants story

--- library/core-components/components/link-icon/link-icon.stories.tsx
+++ library/core-components/components/link-icon/link-icon.stories.tsx
@@ -30,3 +30,67 @@ export default meta;
 type Story = StoryObj<RadiusLinkIconProps>;
 
 export const Default: Story = {};
+
+const Cell = ({ children, id }: { children: React.ReactNode; id?: string }) => (
+  <div
+    id={id}
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+  >
+    {children}
+  </div>
+);
+
+const sizes = {
+  Small: radiusTokens.component.sizing.linkIcon.small,
+  Medium: radiusTokens.component.sizing.linkIcon.medium,
+  Large: radiusTokens.component.sizing.linkIcon.large,
+};
+
+export const Variants: Story = {
+  render: (args) => (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(5, 100px)',
+        gap: '20px',
+        fontFamily: 'Riforma LL',
+        fontSize: '20px',
+      }}
+    >
+      <div></div>
+      <Cell>Normal</Cell>
+      <Cell>Hover</Cell>
+      <Cell>Active</Cell>
+      <Cell>Disabled</Cell>
+
+      {Object.entries(sizes).map(([key, value]) => (
+        <>
+          <Cell>{key}</Cell>
+
+          <Cell>
+            <RadiusLinkIcon {...args} size={value} />
+          </Cell>
+          <Cell id="hover">
+            <RadiusLinkIcon {...args} size={value} />
+          </Cell>
+          <Cell id="active">
+            <RadiusLinkIcon {...args} size={value} />
+          </Cell>
+          <Cell>
+            <RadiusLinkIcon {...args} size={value} disabled />
+          </Cell>
+        </>
+      ))}
+    </div>
+  ),
+  parameters: {
+    pseudo: {
+      hover: ['#hover'],
+      active: ['#active'],
+    },
+  },
+};
