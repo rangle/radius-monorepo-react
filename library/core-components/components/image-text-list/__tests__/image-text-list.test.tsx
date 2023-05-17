13cb2ac201bb5d86b38f58dff75bd89d4d290aa9
Nathan Kanigsberg <nathan@kanigsberg.com>
2023-05-17 09:59:19 -0400

    add tests

--- library/core-components/components/image-text-list/__tests__/image-text-list.test.tsx
+++ library/core-components/components/image-text-list/__tests__/image-text-list.test.tsx
@@ -4,61 +4,68 @@ import '@testing-library/jest-dom';
 import { axe, toHaveNoViolations } from 'jest-axe';
 
 import { RadiusImageTextList } from '..';
-import { RadiusImageTextItemProps } from '../image-text-list.types';
 
 expect.extend(toHaveNoViolations);
 
 describe('<RadiusImageTextList />', () => {
-  // type ExcludeSmallVariant<T> = T extends { variant: 'small' } ? never : T;
-  // type ExcludeLargeVariant<T> = T extends { variant: 'large' } ? never : T;
-  // const renderLargeVariant = (
-  //   props?: Partial<ExcludeSmallVariant<RadiusImageTextItemProps>>
-  // ) => {
-  //   return render(
-  //     <RadiusImageTextItem
-  //       header="Hello World"
-  //       variant="large"
-  //       body="body text"
-  //       headingLevel="h2"
-  //       src="https://via.placeholder.com/1500"
-  //       alt="placeholder image"
-  //       {...props}
-  //     />
-  //   );
-  // };
-  // const renderSmallVariant = (
-  //   props?: Partial<ExcludeLargeVariant<RadiusImageTextItemProps>>
-  // ) => {
-  //   return render(
-  //     <RadiusImageTextItem
-  //       variant="small"
-  //       body="body text"
-  //       src="https://via.placeholder.com/1500"
-  //       alt="placeholder image"
-  //       {...props}
-  //     />
-  //   );
-  // };
-  // test('should render large variant', () => {
-  //   const { getByText } = renderLargeVariant();
-  //   expect(getByText('Hello World')).toBeInTheDocument();
-  // });
-  // test('should render small variant', () => {
-  //   const { getByText } = renderSmallVariant();
-  //   expect(getByText('body text')).toBeInTheDocument();
-  // });
-  // test('large variant should have no accessibility violations', async () => {
-  //   const { container } = renderLargeVariant();
-  //   const results = await axe(container);
-  //   expect(results).toHaveNoViolations();
-  // });
-  // test('small variant should have no accessibility violations', async () => {
-  //   const { container } = renderSmallVariant();
-  //   const results = await axe(container);
-  //   expect(results).toHaveNoViolations();
-  // });
-  // test('should display the correct heading level for large variant', () => {
-  //   const { getByText } = renderLargeVariant({ headingLevel: 'h3' });
-  //   expect(getByText('Hello World').tagName).toBe('H3');
-  // });
+  const renderLargeVariant = () => {
+    return render(
+      <RadiusImageTextList
+        items={[
+          {
+            alt: 'Descriptive text',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+            header: 'Header 1',
+            headingLevel: 'h2',
+            src: 'https://via.placeholder.com/1500',
+          },
+          {
+            alt: 'Descriptive text',
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+            header: 'Header 2',
+            headingLevel: 'h2',
+            src: 'https://via.placeholder.com/1500',
+          },
+        ]}
+        variant="large"
+      />
+    );
+  };
+  const renderSmallVariant = () => {
+    return render(
+      <RadiusImageTextList
+        items={[
+          {
+            alt: 'Descriptive text',
+            body: 'body 1',
+            src: 'https://via.placeholder.com/1500',
+          },
+          {
+            alt: 'Descriptive text',
+            body: 'body 2',
+            src: 'https://via.placeholder.com/1500',
+          },
+        ]}
+        variant="small"
+      />
+    );
+  };
+  test('should render large variant', () => {
+    const { getByText } = renderLargeVariant();
+    expect(getByText('Header 1')).toBeInTheDocument();
+  });
+  test('should render small variant', () => {
+    const { getByText } = renderSmallVariant();
+    expect(getByText('body 1')).toBeInTheDocument();
+  });
+  test('large variant should have no accessibility violations', async () => {
+    const { container } = renderLargeVariant();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+  test('small variant should have no accessibility violations', async () => {
+    const { container } = renderSmallVariant();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
