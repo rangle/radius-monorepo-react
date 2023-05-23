1b063ab699d4dedd9d9379c168f6b89d679bb4ad
Nathan Kanigsberg <nathan.kanigsberg@rangle.io>
2023-05-23 14:40:08 -0400

    Inline Link component (#88)
    * inline-link boilerplate
    
    * add styles
    - still need to figure out size/typography
    
    * add typography
    
    * update storybook
    
    * fix tests
    
    * add description

---
+++ library/core-components/components/inline-link/__tests__/inline-link.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { RadiusInlineLink } from '..';
+import { radiusTokens } from '@rangle/radius-foundations/generated/design-tokens.constants';
+
+expect.extend(toHaveNoViolations);
+
+describe('<RadiusInlineLink />', () => {
+  test('should render', () => {
+    const { getByRole } = render(
+      <RadiusInlineLink
+        href="#"
+        typography={radiusTokens.component.typography.inlineText.label.small}
+      >
+        test
+      </RadiusInlineLink>
+    );
+    expect(getByRole('link')).toBeInTheDocument();
+  });
+
+  test('should have no accessibility violations', async () => {
+    const { container } = render(
+      <RadiusInlineLink
+        href="#"
+        typography={radiusTokens.component.typography.inlineText.label.small}
+      >
+        test
+      </RadiusInlineLink>
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  });
+});
---
+++ library/core-components/components/inline-link/inline-link.tsx
@@ -0,0 +1,62 @@
+import React, { forwardRef } from 'react';
+import { cx } from '@emotion/css';
+import { radiusTokens } from '@rangle/radius-foundations/generated/design-tokens.constants';
+
+import { useStyles } from './inline-link.styles';
+import {
+  RadiusInlineLinkComponent,
+  RadiusInlineLinkProps,
+} from './inline-link.types';
+import { PolymorphicRef } from '../../utils';
+import { Typography } from '../typography/typography';
+
+/**
+ * A component that renders a link as a text element. It is a wrapper around
+ * the `Typography` component, and can be passed text or any valid ReactNode.
+ *
+ * It is polymorphic and can be passed a custom wrapper component via the `as`
+ * prop, eg. using React Router's `Link` component. By default it renders an `a`
+ * tag.
+ *
+ * ### Resources
+ * [Figma Design Specs](https://www.figma.com/file/EUSxceIm71tCWSzPE27BwW/Inline-Link?type=design&node-id=2-2)
+ */
+export const RadiusInlineLink: RadiusInlineLinkComponent = forwardRef(
+  <C extends React.ElementType = 'a'>(
+    {
+      as,
+      children,
+      href,
+      typography,
+      disabled,
+      className,
+      ...rest
+    }: RadiusInlineLinkProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const Component = as || 'a';
+    const { styles, label } = useStyles({ disabled });
+
+    return (
+      <Component
+        className={cx(styles, className)}
+        ref={ref}
+        href={!disabled ? href : undefined}
+        disabled={disabled}
+        {...rest}
+      >
+        <Typography
+          className={label}
+          fill={
+            disabled
+              ? radiusTokens.component.color.inlineText.disabled
+              : radiusTokens.component.color.inlineText.default
+          }
+          {...typography}
+        >
+          {children}
+        </Typography>
+      </Component>
+    );
+  }
+);
