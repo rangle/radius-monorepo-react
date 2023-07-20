2aa6bfce3bdf68dd070e5094ae43af2e1bc3008c
Michael Mrowetz <michael.mrowetz@rangle.io>
2023-07-20 16:17:21 -0400

    docs: imporve assets docs

--- library/foundations/generator-config.ts
+++ library/foundations/generator-config.ts
@@ -1,5 +1,5 @@
 /** Customizable Configuration for the Generator */
 export const config = {
-  /** Responsive image size bounds (max width/ max height) */
+  /** Responsive image size bounds (max width/ max height in px) */
   imageSizes: [400, 600, 800, 1000],
 } as const;
